7a5042d8529abdd129f0aab5fc48104355564297
shinkabasawa <s-kabasawa@haiming.co.jp>
2019-07-04 17:44:23 +0900

    Tab切り替え追加

--- app/src/main/java/haiming/co/jp/sample_02/Activity/FragmentbaseActivity.java
+++ app/src/main/java/haiming/co/jp/sample_02/Activity/FragmentbaseActivity.java
@@ -1,19 +1,26 @@
 package haiming.co.jp.sample_02.Activity;
 
-import android.net.Uri;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.TabHost;
+import android.widget.TabHost.TabSpec;
 
 import haiming.co.jp.sample_02.DialogFragment.sampleDialogFragment;
-import haiming.co.jp.sample_02.Fragment.Fragment1;
+import haiming.co.jp.sample_02.Fragment.Tab1Fragment;
+import haiming.co.jp.sample_02.Fragment.Tab2Fragment;
+import haiming.co.jp.sample_02.Fragment.Tab3Fragment;
 import haiming.co.jp.sample_02.R;
 
-public class FragmentbaseActivity extends AppCompatActivity implements Fragment1.OnFragmentInteractionListener {
+public class FragmentbaseActivity extends AppCompatActivity implements TabHost.OnTabChangeListener {
 
     private FrameLayout frameLayout;
+    private TabHost tabHost;
+    private String mLastTabId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,13 +32,34 @@ public class FragmentbaseActivity extends AppCompatActivity implements Fragment1
     @Override
     protected void onStart(){
         super.onStart();
-        frameLayout = (FrameLayout)findViewById(R.id.framelayout);
         Log.v("FragmentbaseAct","onStart");
-    }
 
-    @Override
-    public void onFragmentInteraction(Uri uri) {
-        Log.v("FragmentbaseAct","onFragmentInteraction");Log.v("FragmentbaseAct","onFragmentInteraction");
+        tabHost = (TabHost) findViewById(android.R.id.tabhost);
+        tabHost.setup();
+        /* Tab1 設定 */
+        TabSpec tab1 = tabHost.newTabSpec("tab1");
+        tab1.setIndicator("TAB1");
+        tab1.setContent(new DummyTabFactory(this));
+        tabHost.addTab(tab1);
+
+        // Tab2 設定
+        TabSpec tab2 = tabHost.newTabSpec("tab2");
+        tab2.setIndicator("TAB2");
+        tab2.setContent(new DummyTabFactory(this));
+        tabHost.addTab(tab2);
+
+        // Tab3 設定
+        TabSpec tab3 = tabHost.newTabSpec("tab3");
+        tab3.setIndicator("TAB3");
+        tab3.setContent(new DummyTabFactory(this));
+        tabHost.addTab(tab3);
+
+        // タブ変更時イベントハンドラ
+        tabHost.setOnTabChangedListener(this);
+        mLastTabId = "tab1";
+        // 初期タブ選択
+
+        tabHost.setCurrentTabByTag("tab1");
     }
 
     public void dialog_btn(View v){
@@ -40,10 +68,52 @@ public class FragmentbaseActivity extends AppCompatActivity implements Fragment1
     }
 
     public void onbtn_onClick(View v){
-        frameLayout.setVisibility(View.VISIBLE);
+        //frameLayout.setVisibility(View.VISIBLE);
     }
 
     public void hide_onClick(View v){
-        frameLayout.setVisibility(View.INVISIBLE);
+        //frameLayout.setVisibility(View.INVISIBLE);
+    }
+
+    @Override
+    public void onTabChanged(String tabId) {
+
+        if (mLastTabId != null) {
+            if (!mLastTabId.equals(tabId)) {
+                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                if ("tab1".equals(tabId)) {
+                    fragmentTransaction.replace(R.id.realtabcontent, new Tab1Fragment());
+                    Log.d("TAB_FRAGMENT_LOG","tabId:" + tabId);
+                } else if ("tab2".equals(tabId)) {
+                    fragmentTransaction.replace(R.id.realtabcontent, new Tab2Fragment());
+                    Log.d("TAB_FRAGMENT_LOG","tabId:" + tabId);
+                } else if ("tab3".equals(tabId)) {
+                    fragmentTransaction.replace(R.id.realtabcontent, new Tab3Fragment());
+                    Log.d("TAB_FRAGMENT_LOG","tabId:" + tabId);
+                }
+                mLastTabId = tabId;
+                fragmentTransaction.commit();
+            }
+        }
+    }
+
+
+    /*
+     * android:id/tabcontent のダミーコンテンツ
+     */
+    private static class DummyTabFactory implements TabHost.TabContentFactory {
+
+        /* Context */
+        private final Context mContext;
+
+        DummyTabFactory(Context context) {
+            mContext = context;
+        }
+
+        @Override
+        public View createTabContent(String tag) {
+            View v = new View(mContext);
+            return v;
+        }
     }
 }
--- app/src/main/java/haiming/co/jp/sample_02/Fragment/Fragment1.java
+++ app/src/main/java/haiming/co/jp/sample_02/Fragment/Fragment1.java
@@ -96,13 +96,6 @@ public class Fragment1 extends Fragment {
         return layoutInflater;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
@@ -133,7 +126,6 @@ public class Fragment1 extends Fragment {
      * >Communicating with Other Fragments</a> for more information.
      */
     public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
 }
