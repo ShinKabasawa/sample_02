0de76dab70ec02e9232e3324de0cec9c07989c8a
shinkabasawa <s-kabasawa@haiming.co.jp>
2019-07-04 17:57:34 +0900

    Tab切り替え追加 　・初期表示tab設定追加

--- app/src/main/java/haiming/co/jp/sample_02/Activity/FragmentbaseActivity.java
+++ app/src/main/java/haiming/co/jp/sample_02/Activity/FragmentbaseActivity.java
@@ -1,14 +1,11 @@
 package haiming.co.jp.sample_02.Activity;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.widget.FrameLayout;
 import android.widget.TabHost;
-import android.widget.TabHost.TabSpec;
 
 import haiming.co.jp.sample_02.DialogFragment.sampleDialogFragment;
 import haiming.co.jp.sample_02.Fragment.Tab1Fragment;
@@ -18,48 +15,46 @@ import haiming.co.jp.sample_02.R;
 
 public class FragmentbaseActivity extends AppCompatActivity implements TabHost.OnTabChangeListener {
 
-    private FrameLayout frameLayout;
     private TabHost tabHost;
-    private String mLastTabId;
+    private String mLastTabId = "tab1";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fragmentbase);
         Log.v("FragmentbaseAct","onCreate");
+        tabHost = (TabHost) findViewById(android.R.id.tabhost);
+        tabHost.setup();
+        /* Tab1 設定 */
+        TabHost.TabSpec tab1 = tabHost.newTabSpec("tab1");
+        tab1.setIndicator("TAB1");
+        tab1.setContent(R.id.tab1);
+        tabHost.addTab(tab1);
+
+        // Tab2 設定
+        TabHost.TabSpec tab2 = tabHost.newTabSpec("tab2");
+        tab2.setIndicator("TAB2");
+        tab2.setContent(R.id.tab2);
+        tabHost.addTab(tab2);
+
+        // Tab3 設定
+        TabHost.TabSpec tab3 = tabHost.newTabSpec("tab3");
+        tab3.setIndicator("TAB3");
+        tab3.setContent(R.id.tab3);
+        tabHost.addTab(tab3);
+        tabHost.setCurrentTab(0);
+        tabHost.setOnTabChangedListener(this);
+        mLastTabId = "tab1";
+
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.realtabcontent, new Tab1Fragment());
+        fragmentTransaction.commit();
     }
 
     @Override
     protected void onStart(){
         super.onStart();
         Log.v("FragmentbaseAct","onStart");
-
-        tabHost = (TabHost) findViewById(android.R.id.tabhost);
-        tabHost.setup();
-        /* Tab1 設定 */
-        TabSpec tab1 = tabHost.newTabSpec("tab1");
-        tab1.setIndicator("TAB1");
-        tab1.setContent(new DummyTabFactory(this));
-        tabHost.addTab(tab1);
-
-        // Tab2 設定
-        TabSpec tab2 = tabHost.newTabSpec("tab2");
-        tab2.setIndicator("TAB2");
-        tab2.setContent(new DummyTabFactory(this));
-        tabHost.addTab(tab2);
-
-        // Tab3 設定
-        TabSpec tab3 = tabHost.newTabSpec("tab3");
-        tab3.setIndicator("TAB3");
-        tab3.setContent(new DummyTabFactory(this));
-        tabHost.addTab(tab3);
-
-        // タブ変更時イベントハンドラ
-        tabHost.setOnTabChangedListener(this);
-        mLastTabId = "tab1";
-        // 初期タブ選択
-
-        tabHost.setCurrentTabByTag("tab1");
     }
 
     public void dialog_btn(View v){
@@ -101,19 +96,19 @@ public class FragmentbaseActivity extends AppCompatActivity implements TabHost.O
     /*
      * android:id/tabcontent のダミーコンテンツ
      */
-    private static class DummyTabFactory implements TabHost.TabContentFactory {
-
-        /* Context */
-        private final Context mContext;
-
-        DummyTabFactory(Context context) {
-            mContext = context;
-        }
-
-        @Override
-        public View createTabContent(String tag) {
-            View v = new View(mContext);
-            return v;
-        }
-    }
+//    private static class DummyTabFactory implements TabHost.TabContentFactory {
+//
+//        /* Context */
+//        private final Context mContext;
+//
+//        DummyTabFactory(Context context) {
+//            mContext = context;
+//        }
+//
+//        @Override
+//        public View createTabContent(String tag) {
+//            View v = new View(mContext);
+//            return v;
+//        }
+//    }
 }
