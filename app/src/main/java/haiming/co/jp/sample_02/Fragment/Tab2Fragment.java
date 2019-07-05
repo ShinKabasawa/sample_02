0902269f172d3fb3fbd692d7b53d2b4d1465e224
shinkabasawa <s-kabasawa@haiming.co.jp>
2019-07-05 13:39:49 +0900

    追加

--- app/src/main/java/haiming/co/jp/sample_02/Fragment/Tab2Fragment.java
+++ app/src/main/java/haiming/co/jp/sample_02/Fragment/Tab2Fragment.java
@@ -8,13 +8,13 @@ import android.view.ViewGroup;
 
 import haiming.co.jp.sample_02.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * to handle interaction events.
- * Use the {@link Tab2Fragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+///**
+// * A simple {@link Fragment} subclass.
+// * Activities that contain this fragment must implement the
+// * to handle interaction events.
+// * Use the {@link Tab2Fragment#newInstance} factory method to
+// * create an instance of this fragment.
+// */
 public class Tab2Fragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -31,14 +31,14 @@ public class Tab2Fragment extends Fragment {
 //        // Required empty public constructor
 //    }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Tab2Fragment.
-     */
+//    /**
+//     * Use this factory method to create a new instance of
+//     * this fragment using the provided parameters.
+//     *
+//     * @param param1 Parameter 1.
+//     * @param param2 Parameter 2.
+//     * @return A new instance of fragment Tab2Fragment.
+//     */
 //    // TODO: Rename and change types and number of parameters
 //    public static Tab2Fragment newInstance(String param1, String param2) {
 //        Tab2Fragment fragment = new Tab2Fragment();
--- app/src/main/java/haiming/co/jp/sample_02/Fragment/Tab3Fragment.java
+++ app/src/main/java/haiming/co/jp/sample_02/Fragment/Tab3Fragment.java
@@ -8,14 +8,14 @@ import android.view.ViewGroup;
 
 import haiming.co.jp.sample_02.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link Tab3Fragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link Tab3Fragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+///**
+// * A simple {@link Fragment} subclass.
+// * Activities that contain this fragment must implement the
+// * {@link Tab3Fragment.OnFragmentInteractionListener} interface
+// * to handle interaction events.
+// * Use the {@link Tab3Fragment#newInstance} factory method to
+// * create an instance of this fragment.
+// */
 public class Tab3Fragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -32,14 +32,14 @@ public class Tab3Fragment extends Fragment {
 //        // Required empty public constructor
 //    }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Tab3Fragment.
-     */
+//    /**
+//     * Use this factory method to create a new instance of
+//     * this fragment using the provided parameters.
+//     *
+//     * @param param1 Parameter 1.
+//     * @param param2 Parameter 2.
+//     * @return A new instance of fragment Tab3Fragment.
+//     */
     // TODO: Rename and change types and number of parameters
 //    public static Tab3Fragment newInstance(String param1, String param2) {
 //        Tab3Fragment fragment = new Tab3Fragment();
