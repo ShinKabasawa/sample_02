1ed888c3dabfb5b20b2c24491b33cb3196fae831
shinkabasawa <s-kabasawa@haiming.co.jp>
2019-07-05 17:17:06 +0900

    FragmentSampleActivity追加

--- app/src/main/java/haiming/co/jp/sample_02/Fragment/Fragment1.java
+++ app/src/main/java/haiming/co/jp/sample_02/Fragment/Fragment1.java
@@ -22,40 +22,10 @@ import haiming.co.jp.sample_02.R;
  * create an instance of this fragment.
  */
 public class Fragment1 extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
-    private String mParam2;
 
     private OnFragmentInteractionListener mListener;
-
-    public static Fragment1 getInstance(Bundle bundle){
-        Log.v("Fragment1","getInstance");
-        Fragment1 instance = new Fragment1();
-        return instance;
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Fragment1.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Fragment1 newInstance(String param1, String param2) {
-        Fragment1 fragment = new Fragment1();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/haiming/co/jp/sample_02/Fragment/Tab3Fragment.java
+++ app/src/main/java/haiming/co/jp/sample_02/Fragment/Tab3Fragment.java
@@ -11,8 +11,7 @@ import haiming.co.jp.sample_02.R;
 public class Tab3Fragment extends Fragment {
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_tab3, container, false);
         return v;
