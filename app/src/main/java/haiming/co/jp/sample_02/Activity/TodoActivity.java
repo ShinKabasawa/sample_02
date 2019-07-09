e188b814744c7e9805fa8f284bbefb062212c85d
shinkabasawa <s-kabasawa@haiming.co.jp>
2019-07-09 16:06:44 +0900

    TodoActivity・TodoregistActivity追加

---
+++ app/src/main/java/haiming/co/jp/sample_02/Activity/TodoActivity.java
@@ -0,0 +1,15 @@
+package haiming.co.jp.sample_02.Activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import haiming.co.jp.sample_02.R;
+
+public class TodoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_todo);
+    }
+}
