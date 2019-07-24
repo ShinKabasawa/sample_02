a2d5b8bacbb74c631004c581def57a9b6f61daf4
shinkabasawa <s-kabasawa@haiming.co.jp>
2019-07-24 17:08:40 +0900

    位置情報取得追加

--- app/src/main/java/haiming/co/jp/sample_02/Activity/Main2Activity.java
+++ app/src/main/java/haiming/co/jp/sample_02/Activity/Main2Activity.java
@@ -1,22 +1,40 @@
 package haiming.co.jp.sample_02.Activity;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.content.res.Resources;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.FusedLocationProviderApi;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import haiming.co.jp.sample_02.Data.Common;
 import haiming.co.jp.sample_02.Data.prefectures_data;
 import haiming.co.jp.sample_02.Manager.FileManager;
 import haiming.co.jp.sample_02.R;
 
-public class Main2Activity extends AppCompatActivity {
+public class Main2Activity extends AppCompatActivity implements LocationListener {
 
     private Spinner spinner_1;
     private Spinner spinner_2;
@@ -26,21 +44,25 @@ public class Main2Activity extends AppCompatActivity {
     private String place;
     private String pref;
     private String city;
+    private LocationManager locationManager;
+//    private GoogleApiClient googleApiClient;
+    private GoogleApiClient _apiClient;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
     }
 
-    protected void onStart(){
+    protected void onStart() {
         super.onStart();
         init();
 
         read_file();
 
-        String[] spinneritem_1 = {"自宅","勤務先","旅行先1","旅行先"};
+        String[] spinneritem_1 = {"自宅", "勤務先", "旅行先1", "旅行先2"};
 
         ArrayAdapter<String> adapter_1 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, spinneritem_1);
         adapter_1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -52,36 +74,60 @@ public class Main2Activity extends AppCompatActivity {
     }
 
 
-    private void init(){
-        spinner_1 = (Spinner)findViewById(R.id.spinner_1);
-        spinner_2 = (Spinner)findViewById(R.id.spinner_2);
-        spinner_3 = (Spinner)findViewById(R.id.spinner_3);
+    /**
+     * 初期化
+     */
+    private void init() {
+        place = "";
+        pref = "";
+        city = "";
+        spinner_1 = (Spinner) findViewById(R.id.spinner_1);
+        spinner_2 = (Spinner) findViewById(R.id.spinner_2);
+        spinner_3 = (Spinner) findViewById(R.id.spinner_3);
 
         spinner_1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                place = (String)spinner_1.getSelectedItem();
+                place = (String) spinner_1.getSelectedItem();
+                Common.loadPreference(getApplicationContext(), place, 1);
+                pref = (String) Common.loadPreference(getApplicationContext(), "自宅pref", 1);
+                city = (String) Common.loadPreference(getApplicationContext(), "自宅city", 1);
+
+//                int cnt = 0;
+//                ArrayList<String> arrayList = new ArrayList<>();
+//                for (int i = 0 ; i< pref_name_array.size(); i++){
+//                    if (pref.equals(pref_name_array.get(i))){
+//                        cnt = i;
+//                        break;
+//                    }
+//                }
+//
+//                spinner_2.setSelection(cnt);
+
             }
 
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
-                place = (String)spinner_1.getSelectedItem();
+                place = (String) spinner_1.getSelectedItem();
+                Common.loadPreference(getApplicationContext(), place, 1);
+                pref = (String) Common.loadPreference(getApplicationContext(), "自宅pref", 1);
+                city = (String) Common.loadPreference(getApplicationContext(), "自宅city", 1);
             }
         });
 
         spinner_2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                pref = (String)spinner_2.getSelectedItem();
+                pref = (String) spinner_2.getSelectedItem();
 
                 ArrayList<String> arrayList = new ArrayList<>();
 
-                for (int i = 0 ; i< prefe_data_array.size(); i++){
-                    if (pref.equals(prefe_data_array.get(i).prefectures)){
-                        prefectures_data prefectures_data = new prefectures_data();
+                for (int i = 0; i < prefe_data_array.size(); i++) {
+                    if (pref.equals(prefe_data_array.get(i).prefectures)) {
                         arrayList.add(prefe_data_array.get(i).city);
                     }
                 }
+
                 ArrayAdapter<String> adapter_3 = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_spinner_item, arrayList);
                 adapter_3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
                 spinner_3.setAdapter(adapter_3);
@@ -89,12 +135,12 @@ public class Main2Activity extends AppCompatActivity {
 
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
-                pref = (String)spinner_2.getSelectedItem();
+                pref = (String) spinner_2.getSelectedItem();
 
                 ArrayList<String> arrayList = new ArrayList<>();
 
-                for (int i = 0 ; i< prefe_data_array.size(); i++){
-                    if (pref.equals(prefe_data_array.get(i).prefectures)){
+                for (int i = 0; i < prefe_data_array.size(); i++) {
+                    if (pref.equals(prefe_data_array.get(i).prefectures)) {
                         prefectures_data prefectures_data = new prefectures_data();
                         arrayList.add(prefe_data_array.get(i).city);
                     }
@@ -108,37 +154,235 @@ public class Main2Activity extends AppCompatActivity {
         spinner_3.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                city = (String)spinner_3.getSelectedItem();
+                city = (String) spinner_3.getSelectedItem();
             }
 
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
-                city = (String)spinner_3.getSelectedItem();
+                city = (String) spinner_3.getSelectedItem();
             }
         });
 
         prefe_data_array = new ArrayList<>();
         pref_name_array = new ArrayList<>();
-        place = "";
-        pref = "";
-        city = "";
+
+    }
+
+    private void enableLoc() {
+
+//        if (googleApiClient == null) {
+//            googleApiClient = new GoogleApiClient.Builder(Main2Activity.this)
+//                    .addApi(LocationServices.API)
+//                    .addConnectionCallbacks(new GoogleApiClient.ConnectionCallbacks() {
+//                        @Override
+//                        public void onConnected(Bundle bundle) {
+//                            Log.v("########","onConnected");
+//                        }
+//
+//                        @Override
+//                        public void onConnectionSuspended(int i) {
+//                            Log.v("######","onConnectionSuspended");
+//                            googleApiClient.connect();
+//                        }
+//                    })
+//                    .addOnConnectionFailedListener(new GoogleApiClient.OnConnectionFailedListener() {
+//                        @Override
+//                        public void onConnectionFailed(ConnectionResult connectionResult) {
+//
+//                            Log.d("Location error", "Location error " + connectionResult.getErrorCode());
+//                        }
+//                    }).build();
+//            googleApiClient.connect();
+//        }
+        LocationRequest locationRequest = LocationRequest.create();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(30 * 1000);
+        locationRequest.setFastestInterval(5 * 1000);
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder().addLocationRequest(locationRequest);
+
+        builder.setAlwaysShow(true);
+
+//        PendingResult<LocationSettingsResult> result = LocationServices.SettingsApi.checkLocationSettings(googleApiClient, builder.build());
+//        PendingResult<LocationSettingsResult> result = (PendingResult<LocationSettingsResult>) LocationServices.FusedLocationApi;
+//        result.setResultCallback(new ResultCallback<LocationSettingsResult>() {
+//            @Override
+//            public void onResult(LocationSettingsResult result) {
+//                final Status status = result.getStatus();
+//                switch (status.getStatusCode()) {
+//                    case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+//                        try {
+//                            // Show the dialog by calling startResolutionForResult(),
+//                            // and check the result in onActivityResult().
+//                            status.startResolutionForResult(Main2Activity.this, 199);
+//
+//                            finish();
+//                        } catch (Exception e) {
+//                            // Ignore the error.
+//                        }
+//                        break;
+//                }
+//            }
+//        });
     }
 
 
+    // 位置情報許可の確認
+    public void checkPermission(View v) {
+        // 既に許可している
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+
+
+            // 位置情報取得
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                    ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                Log.v("checkSelfPermission", "");
+                return;
+            }
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 500, 1, this);
+
+        }
+        // 拒否していた場合
+        else {
+            requestLocationPermission();
+        }
+    }
+
+    // 許可を求める
+    private void requestLocationPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+            ActivityCompat.requestPermissions(Main2Activity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1000);
+
+        } else {
+            Toast toast = Toast.makeText(this, "許可されないとアプリが実行できません", Toast.LENGTH_SHORT);
+            toast.show();
+
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,}, 1000);
+
+        }
+    }
+
+    // 結果の受け取り
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == 1000) {
+            // 使用が許可された
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(getApplicationContext(), "許可", Toast.LENGTH_SHORT).show();
+
+                _apiClient = new GoogleApiClient.Builder(getApplicationContext())
+                        .addApi(LocationServices.API)
+                        .addConnectionCallbacks(new GoogleApiClient.ConnectionCallbacks() {
+                            @Override
+                            public void onConnected( Bundle bundle) {
+                                //...
+                            }
+
+                            @Override
+                            public void onConnectionSuspended(int i) {
+                                //...
+                            }
+                        })
+                        .addOnConnectionFailedListener(new GoogleApiClient.OnConnectionFailedListener() {
+                            @Override
+                            public void onConnectionFailed( ConnectionResult connectionResult) {
+                                //...
+                            }
+                        }).build();
+
+                FusedLocationProviderApi fused = LocationServices.FusedLocationApi;
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                    ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+
+                LocationRequest request = LocationRequest.create();
+                LocationListener _listener = new LocationListener() {
+                    @Override
+                    public void onLocationChanged(Location location) {
+                        //...
+
+                        String msg = "Lat=" + location.getLatitude() + "Lng=" + location.getLongitude();
+                        Log.d("onLocationChanged", msg);
+                    }
+
+                    @Override
+                    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+                    }
+
+                    @Override
+                    public void onProviderEnabled(String provider) {
+
+                    }
+
+                    @Override
+                    public void onProviderDisabled(String provider) {
+
+                    }
+                };
+
+
+                enableLoc();
+
+                Location location = fused.getLastLocation(_apiClient);
+//                //enableLoc();
+//
+                Log.v("########","lat = "+ location.getLatitude());
+                Log.v("########","lon = "+ location.getLongitude());
+
+                startActivity(new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+                // 位置情報取得
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                    ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    Log.v("checkSelfPermission","");
+                    return;
+                }
+                locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,500, 1, this);
+            } else {
+                // それでも拒否された時の対応
+                Toast toast = Toast.makeText(this, "これ以上なにもできません", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        }
+    }
+
+
+    public void regbtn_onClick(final View v) {
+        String lat = "";
+        String lon = "";
+
+        if (!place.equals("") && !pref.equals("") && !city.equals("")) {
+            for (int i = 0; i < prefe_data_array.size(); i++) {
+                if (city.equals(prefe_data_array.get(i).city)) {
+                    lat = prefe_data_array.get(i).lat;
+                    lon = prefe_data_array.get(i).lon;
+                }
+            }
+
+            Common.setPreference(this, place, city, 1);
+            Common.setPreference(this, place + "pref", pref, 1);
+            Common.setPreference(this, place + "lat", lat, 1);
+            Common.setPreference(this, place + "lon", lon, 1);
+
+
+        } else {
+            Toast.makeText(this, "選択してください", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /**
+     * CSVファイル読込
+     */
     private void read_file(){
         prefe_data_array = (ArrayList<prefectures_data>) FileManager.prefectures_read_file(this);
         Log.v("Main2Activity","prefe_data_array.size = " + prefe_data_array.size());
-
-        //TextView textView = findViewById(R.id.textViewconfirm);
-        //textView.setMovementMethod(ScrollingMovementMethod.getInstance());
-        //StringBuilder value = new StringBuilder();
-        //for (int i= 0; i < prefe_data_array.size();i++){
-        //    value.append(prefe_data_array.get(i).prefectures);
-        //    value.append(" ");
-        //    value.append(prefe_data_array.get(i).city);
-        //    value.append("\n");
-        //}
-        //textView.setText(value.toString());
 
         Resources res = getResources();
         String[] ta = res.getStringArray(R.array.ken_name_array);
@@ -146,5 +390,25 @@ public class Main2Activity extends AppCompatActivity {
     }
 
 
+    @Override
+    public void onLocationChanged(Location location) {
+        String msg = "Lat=" + location.getLatitude() + "Lng=" + location.getLongitude();
+        Log.d("GPS", msg);
+        locationManager.removeUpdates(this);
+    }
 
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        Log.d("GPS", "onStatusChanged");
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        Log.d("GPS", "onProviderEnabled");
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        Log.d("GPS", "onProviderDisabled");
+    }
 }
--- app/src/main/java/haiming/co/jp/sample_02/Data/Common.java
+++ app/src/main/java/haiming/co/jp/sample_02/Data/Common.java
@@ -1,8 +1,10 @@
 package haiming.co.jp.sample_02.Data;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.preference.PreferenceManager;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -37,4 +39,70 @@ public class Common {
         }
         return  result;
     }
+
+    /**
+     * SharedPreferenceを保存する
+     * @param con    Context
+     * @param key    保存するSharedPreferenceのキー
+     * @param obj    保存するSharedPreferenceの値
+     * @param spType 保存するSharedPreferenceの値の型 1:String 2:int 3:boolean 4:Long 5:Float
+     */
+    public static void setPreference(Context con, String key, Object obj, int spType) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(con);
+        String str = String.valueOf(obj);
+
+        switch (spType) {
+            case 1: // String
+                prefs.edit().putString(key, str).apply();
+                break;
+            case 2: // int
+                prefs.edit().putInt(key, Integer.parseInt(str)).apply();
+                break;
+            case 3: // boolean
+                boolean bool = true;
+                if (str.equals("false")) {
+                    bool = false;
+                }
+                prefs.edit().putBoolean(key, bool).apply();
+                break;
+            case 4: // long
+                prefs.edit().putLong(key, Long.parseLong(str)).apply();
+                break;
+            case 5: // float
+                prefs.edit().putFloat(key, Float.parseFloat(str)).apply();
+                break;
+        }
+    }
+    /**
+     * SharedPreferenceをから値を取得する
+     * @param con     Context
+     * @param key     取得するSharedPreferenceのキー
+     * @param spType  取得するSharedPreferenceの値の型
+     * @return        取得したSharedPreferenceの値
+     */
+    public static Object loadPreference(Context con, String key, int spType) {
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(con);
+        Object obj = new Object();
+
+        switch (spType) {
+            case 1: // String
+                obj = sp.getString(key, "");
+                break;
+            case 2: // int
+                obj = sp.getInt(key, 1);
+                break;
+            case 3: // boolean
+                obj = sp.getBoolean(key, false);
+                break;
+            case 4: // long
+                obj = sp.getLong(key, 1);
+                break;
+            case 5: // float
+                obj = sp.getFloat(key, 1);
+                break;
+        }
+        return obj;
+    }
+
+
 }
